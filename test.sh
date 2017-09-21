3a1d520eaba37d90051c0d2be15d3dc296e1e00e
Lukas Vlcek <lukas.vlcek@gmail.com>
2017-09-21 11:06:07 +0200

    Tail ES log

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ set -ex
 
 # Give Elasticsearch some time to start up
 sleep 15
+tail ${ES_HOME}/logs/elasticsearch.log
 
 curl --insecure https://localhost:9200
 curl --insecure https://localhost:9200/_prometheus/metrics | tail -n 20
