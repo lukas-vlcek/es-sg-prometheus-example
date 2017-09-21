475a0b0292aa3fa68afd008dadcb9a222ceaa4aa
Lukas Vlcek <lukas.vlcek@gmail.com>
2017-09-21 17:04:50 +0200

    Add prometheus user into internal and add it to test

--- test.sh
+++ test.sh
@@ -2,5 +2,9 @@
 
 set -ex
 
+# Access REST API as an kirk user who is admin
 curl -sS --insecure -u kirk:kirk https://localhost:9200
-curl -sS --insecure -u kirk:kirk https://localhost:9200/_prometheus/metrics | tail -n 20
+curl -sS --insecure -u kirk:kirk https://localhost:9200/_prometheus/metrics | tail -n 10
+
+# Access REST API as an prometheus user
+curl -sS --insecure -u prometheus:test https://localhost:9200/_prometheus/metrics | tail -n 10
