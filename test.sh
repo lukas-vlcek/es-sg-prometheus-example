cdc6edc4f40e345c6893a4d20e79f54df113f0bc
Lukas Vlcek <lukas.vlcek@gmail.com>
2017-09-22 11:09:28 +0200

    Using foo user

--- test.sh
+++ test.sh
@@ -9,5 +9,5 @@ curl -sS --insecure -u kirk:kirk https://localhost:9200/_prometheus/metrics | ta
 # Access promtheus REST API as 'prometheus' user (is allowed)
 curl -sS --insecure -u prometheus:test https://localhost:9200/_prometheus/metrics | tail -n 10
 
-# Access prometheus REST API as 'test' user (is not allowed)
-curl -sS --insecure -u test:test https://localhost:9200/_prometheus/metrics | tail -n 10
+# Access prometheus REST API as 'foo' user (is not allowed)
+curl -sS --insecure -u foo:test https://localhost:9200/_prometheus/metrics | tail -n 10
