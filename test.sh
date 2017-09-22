b73a873472f3d9da9127ebc7c084a8f193aeb485
Lukas Vlcek <lukas.vlcek@gmail.com>
2017-09-22 13:31:36 +0200

    Add cluster:monitor/nodes/stats role

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-# Access REST API as an 'kirk' user who is an admin
-curl -sS --insecure -u kirk:kirk https://localhost:9200
-curl -sS --insecure -u kirk:kirk https://localhost:9200/_prometheus/metrics | tail -n 10
+# Access REST API as an 'admin'
+curl -sS --insecure -u admin:admin https://localhost:9200
+curl -sS --insecure -u admin:admin https://localhost:9200/_prometheus/metrics | tail -n 10
 
 # Access promtheus REST API as 'prometheus' user (is allowed)
 curl -sS --insecure -u prometheus:test https://localhost:9200/_prometheus/metrics | tail -n 10
