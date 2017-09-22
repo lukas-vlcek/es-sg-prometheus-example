ec66171ad430df3c3dd4a8a751905bdb658e7a38
Lukas Vlcek <lukas.vlcek@gmail.com>
2017-09-22 10:56:41 +0200

    Test the test user

--- test.sh
+++ test.sh
@@ -2,9 +2,12 @@
 
 set -ex
 
-# Access REST API as an kirk user who is admin
+# Access REST API as an 'kirk' user who is an admin
 curl -sS --insecure -u kirk:kirk https://localhost:9200
 curl -sS --insecure -u kirk:kirk https://localhost:9200/_prometheus/metrics | tail -n 10
 
-# Access REST API as an prometheus user
+# Access promtheus REST API as 'prometheus' user (is allowed)
 curl -sS --insecure -u prometheus:test https://localhost:9200/_prometheus/metrics | tail -n 10
+
+# Access prometheus REST API as 'test' user (is not allowed)
+curl -sS --insecure -u test:test https://localhost:9200/_prometheus/metrics | tail -n 10
