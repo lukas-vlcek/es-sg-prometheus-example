4061c9caac41dcb20e5caac4c5ba4edbbcb41f33
Lukas Vlcek <lukas.vlcek@gmail.com>
2017-09-26 11:35:04 +0200

    Order 1

--- test.sh
+++ test.sh
@@ -2,13 +2,6 @@
 
 set -ex
 
-echo Access REST API as an 'admin'
-curl -sS --insecure -u admin:admin 'https://localhost:9200/_cat/plugins?v'
-#curl -sS --insecure -u admin:admin 'https://localhost:9200/_cat/indices?v'
-#curl -sS --insecure -u admin:admin 'https://localhost:9200/searchguard/_search?pretty'
-curl -sS --insecure -u admin:admin https://localhost:9200/_prometheus/metrics | tail -n 5
-echo
-
 echo Access REST API as 'test' user
 #curl -sS --insecure -u test:test https://localhost:9200/_cluster/health
 curl -sS --insecure -u test:test https://localhost:9200/_prometheus/metrics | tail -n 5
@@ -22,9 +15,4 @@ echo
 echo Access REST API as 'prometheus' user
 #curl -sS --insecure -u prometheus:test https://localhost:9200/_cluster/health
 curl -sS --insecure -u prometheus:test https://localhost:9200/_prometheus/metrics | tail -n 5
-echo
-
-echo Access REST API as 'john' user 
-#curl -sS --insecure -u john:doe https://localhost:9200/_cluster/health
-curl -sS --insecure -u john:doe https://localhost:9200/_prometheus/metrics | tail -n 5
 echo
