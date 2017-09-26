2cf621c97150bc6ae209a2cdba575a116309a6a3
Lukas Vlcek <lukas.vlcek@gmail.com>
2017-09-26 11:36:26 +0200

    Order 2

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-echo Access REST API as 'test' user
-#curl -sS --insecure -u test:test https://localhost:9200/_cluster/health
-curl -sS --insecure -u test:test https://localhost:9200/_prometheus/metrics | tail -n 5
+echo Access REST API as 'prometheus' user
+#curl -sS --insecure -u prometheus:test https://localhost:9200/_cluster/health
+curl -sS --insecure -u prometheus:test https://localhost:9200/_prometheus/metrics | tail -n 5
 echo
 
 echo Access REST API as 'foo' user
@@ -12,7 +12,7 @@ echo Access REST API as 'foo' user
 curl -sS --insecure -u foo:test https://localhost:9200/_prometheus/metrics | tail -n 5
 echo
 
-echo Access REST API as 'prometheus' user
-#curl -sS --insecure -u prometheus:test https://localhost:9200/_cluster/health
-curl -sS --insecure -u prometheus:test https://localhost:9200/_prometheus/metrics | tail -n 5
+echo Access REST API as 'test' user
+#curl -sS --insecure -u test:test https://localhost:9200/_cluster/health
+curl -sS --insecure -u test:test https://localhost:9200/_prometheus/metrics | tail -n 5
 echo
