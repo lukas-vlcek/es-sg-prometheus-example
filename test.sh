526b1015c7cf7a094c38dfa8a1bb66e783f7d5d2
Lukáš Vlček <lukas.vlcek@gmail.com>
2017-09-27 09:58:40 +0200

    WIP

--- test.sh
+++ test.sh
@@ -3,16 +3,19 @@
 set -ex
 
 echo Access REST API as 'prometheus' user
+#curl -sS --insecure -u prometheus:test https://localhost:9200/_prometheus/metrics | tail -n 5
 #curl -sS --insecure -u prometheus:test https://localhost:9200/_cluster/health
-curl -sS --insecure -u prometheus:test https://localhost:9200/_prometheus/metrics | tail -n 5
+#curl -sS --insecure -u prometheus:test https://localhost:9200/_nodes/stats | tail -n 1 | sed -E "s/(.{200}).*$/\1/"
 echo
 
 echo Access REST API as 'foo' user
-#curl -sS --insecure -u foo:test https://localhost:9200/_cluster/health
 curl -sS --insecure -u foo:test https://localhost:9200/_prometheus/metrics | tail -n 5
+#curl -sS --insecure -u foo:test https://localhost:9200/_cluster/health
+#curl -sS --insecure -u foo:test https://localhost:9200/_nodes/stats | tail -n 1 | sed -E "s/(.{200}).*$/\1/"
 echo
 
 echo Access REST API as 'test' user
-#curl -sS --insecure -u test:test https://localhost:9200/_cluster/health
 curl -sS --insecure -u test:test https://localhost:9200/_prometheus/metrics | tail -n 5
+#curl -sS --insecure -u test:test https://localhost:9200/_cluster/health
+#curl -sS --insecure -u test:test https://localhost:9200/_nodes/stats | tail -n 1 | sed -E "s/(.{200}).*$/\1/"
 echo
